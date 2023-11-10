35d01bd05170aedc38932dd673fbc0a1d3af4ca5
Dinhhieu202x <dinhhieuvt28122002@gmail.com>
2023-11-10 18:14:10 +0700

    chia lai Package

--- Dictionary/src/Application/APP.java
+++ Dictionary/src/Application/APP.java
@@ -3,6 +3,7 @@ package Application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,9 @@ public class APP extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(APP.class.getResource("commandline.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+        Image icon = new Image("img/icon.png");
+        stage.setTitle("ENG-VIE Dictionary");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
